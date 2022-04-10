43bbc82f1cb62a8042ee94c54f3d660f24a2fb93
rmcph <97561465+rmcph@users.noreply.github.com>
2022-04-10 08:05:29 -0400

    Add files via upload

---
+++ poemquiz.py
@@ -0,0 +1,79 @@
+blank1 = input("When in the ")
+if blank1 == "Course":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+blank2 = input("of human events, it becomes ")
+if blank2 == "necessary":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+blank3 = input("for one people to ")
+if blank3 == "dissolve":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+blank4 = input("the political bands which have ")
+if blank4 == "connected":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+blank5 = input("them to another, and to ")
+if blank5 == "assume":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+blank6 = input("among the powers of the ")
+if blank6 == "Earth":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+blank7 = input("the separate and equal ")
+if blank7 == "station":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+blank8 = input("to which the Laws of Nature and of Nature's ")
+if blank8 == "God":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+blank9 = input("entitle them, a decent ")
+if blank9 == "respect":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+blank10 = input("to the opinions of mankind ")
+if blank10 == "requires":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+blank11 = input("that they should declare the causes which ")
+if blank11 == "impel":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+blank12 = input("them to the ")
+if blank12 == "separation":
+    print ("You are correct!")
+else:
+    print ("No, that is not correct.  Keep studying!")
+
+print("Here is your completed quiz:")
+print("When in the " + blank1 + " of human events, it becomes " + blank2 + " for one people to " \
++ blank3 + " the political bands which have " + blank4 + " them with another, and to " + blank5 + \
+" among the powers of the " + blank6 + ", the separate and equal " + blank7 + \
+" to which the Laws of Nature and of Nature's " + blank8 + " entitle them, a decent " + blank9 + \
+" to the opinions of mankind " + blank10 + " that they should declare the causes which " + blank11 + \
+" them to the " + blank12 + ".")
